7770a57163a5fe496950c968b6221c23991a604e
amarao <amarao@home>
2014-06-02 22:26:56 +0300

    pep8 cleanup, readme update

--- README.md
+++ README.md
@@ -3,7 +3,42 @@ json4shell
 
 Set of utilities for shell for json processing
 
-(initial work in progress)
+(initial work in progress, do not use in production. Seriously!)
 
-See http://amarao-san.livejournal.com/2813949.html for whole idea (Russian)
+See 
+* http://amarao-san.livejournal.com/2813949.html for whole idea (Russian)
+* http://habrahabr.ru/post/102072 public idea (Russian)
+
+Developer notes:
+
+1. Contribution is welcome!
+2. Any module is ok if it does not create additional dependences or force additional rules on other modules.
+3. use pep8 on file before commiting (apt-get install pep8, pep8 *.py)
+
+Internal architecture:
+
+For development purposes commands are added as arg[1]. Later, selection will be moved to arg[0] and symlinks 
+with different names to same binary.
+
+Every module should use own argparse code. This can be changed in future to help support common options.
+
+Common rules:
+1. Output (except 'outputs') should be json only to stdout. Any complains may be send to stderr.
+2. Exit codes:
+    On success exit code is 0
+    On critical failure (invalid options) exit code should be -1
+    On unexpected file close exit code should be 2
+    On invalid input exit code should be 3
+    Rest of codes - for command-specific cases
+3. Input (except 'input' type should be expected as one or more JSONs, separated by any combination of spaces, 
+tabs and line feeds.
+4. If few json's are in input, they should be threated as single array of elements (output can be few JSONs or
+one JSON with array at you preferences)
+
+
+Types of commands:
+1. Inputs: take something (from stdin or from external datasource) and output json to stdout.
+2. Filters: take json on stdin, return changed json to stdout.
+3. Iterators: execute other commands on some (every?) element of JSON, return commands output instead of elements.
+4. Outputs: take JSON, return something, depends on internals.
 
--- jsonops.py
+++ jsonops.py
@@ -1,58 +1,67 @@
 #!/usr/bin/python
 '''
-	JSON ops - operation on json
-	Every function accepts:
-		j - json object
-		args - defaultdict with options (None if no option defined)
-	Every function returns processed json or raise exception
+    JSON ops - operation on json
+    Every function accepts:
+        j - json object
+        args - defaultdict with options (None if no option defined)
+    Every function returns processed json or raise exception
 '''
 
+
 def array_slice(j, args):
-    argument = args[0] #The rest of arguments is not needed
-    #argument is something like "[0:10]", let's try parsing that
+    argument = args[0]  # The rest of arguments is not needed
+    # argument is something like "0:10", let's try parsing that
     try:
         start, end = [int(x) for x in argument[1:-1].split(":")]
     except:
-        raise #Need to define which exception is to be caught and which is to be thrown here
+        raise
+        # Need to define which exception is
+        # to be caught and which is to be thrown here
     if start > len(j):
-        start=len(j)
-    if end >len(j):
-        end=len(j)
+        start = len(j)
+    if end > len(j):
+        end = len(j)
     return j[start:end]
 
-def array_head(j,args):
+
+def array_head(j, args):
     try:
         end = int(args[0])
     except:
-        raise #Same here, need to see which exception is suitable here
+        raise  # Same here, need to see which exception is suitable here
     result = j[:end]
     return result
 
-def array_tail(j,args):
+
+def array_tail(j, args):
     try:
         start = int(args[0])
     except:
-        raise #Same here, need to see which exception is suitable here
+        raise  # Same here, need to see which exception is suitable here
     result = j[start:]
     return result
 
-def array_get(j,args):
+
+def array_get(j, args):
     try:
         position = int(args[0])
     except:
-        raise #Same here, need to see which exception is suitable here
+        raise  # Same here, need to see which exception is suitable here
     result = j[position]
     return result
 
-def array_uniq(j,args):
-    #Can't think of any possible arguments now
+
+def array_uniq(j, args):
+    # Can't think of any possible arguments now
     seen = {}
     result = []
     for item in j:
-        if item in seen: continue
+        if item in seen:
+            continue
         seen[item] = 1
         result.append(item)
     return result
+
 
 def array_shuffle(j, args):
     from random import shuffle
@@ -60,8 +69,10 @@ def array_shuffle(j, args):
     shuffle(j)
     return j
 
-def array_sort(j,args):
-    #TODO - implement string sorting according to language - will need to ompletely revise
+
+def array_sort(j, args):
+    #TODO - implement string sorting
+    #according to language - will need to ompletely revise
     if "reversed" in args:
         j.sort(reverse=True)
     else:
